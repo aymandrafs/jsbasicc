e40a74bf03e2b74f3d116fa632dccb58319a6cde
aymankhan89 <167996547+aymankhan89@users.noreply.github.com>
2024-05-15 22:27:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+// let count = 5;
+// document.getElementById
+// console.log(count);
+
+// let myAge = 10;
+
+// console.log(myAge);
+
+// let myAge = 10;
+// let HumanDogRatio = 14 ;
+
+// let myDogAge =  myAge * HumanDogRatio ;
+
+// console.log(myDogAge);
+
+// let bonusPoints = 50; 
+// console.log(bonusPoints);
+
+//  bonusPoints = bonusPoints + 50;
+//  console.log(bonusPoints);
+
+//  bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+//  bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// function myFirstFunction(){
+//     console.log(45);
+// }
+// myFirstFunction()
+// function lap(){
+//     let lap1 = 34; 
+//     let lap2 = 33;
+//     let lap3 = 36;
+//     console.log(lap1 + lap2 + lap3);
+// }
+// lap()
+
+// lapsCompleted = 0;
+
+// function incrementLap(){
+//     lapsCompleted = lapsCompleted + 1
+// }
+// incrementLap()
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted)
+
+
+// let countEl = document.getElementById ("count-el");
+// console.log(countEl);
+
+
+//  let count = 5 ;
+//  function increment() {
+// countEl.innerText = count;
+//     count = count + 1 ;
+//  }
+
+//  let message = "you have three new notification"
+//  let username = "per"
+// // console.log(message);
+//  let messageToUser = message + ", " + username +  "!" ;
+
+
+//  console.log(messageToUser);
+
+
+// let name = "ayman"
+// let greeting = "hi"
+//   let myGreeting =  name +  greeting;
+
+// console.log(myGreeting);
+
+
+
+
+
+
+
+
+
+
+let welcomeEl = document.getElementById("welcome-el");
+console.log(welcomeEl);
+let name = "ayman"
+let greeting = "hi"
+  welcomeEl.innerText = name +  greeting;
+
+
+
+// let countEl = document.getElementById ("count-el");
+
+// console.log(countEl);
+// let count = 0 ;
+
+// function increment(){
+   
+//     count = count + 1;
+//     countEl.innerText = count ;
+// }
+
+// function save(){
+//     console.log(count);
+// }
